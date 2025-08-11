557d3e2fab3035c68aa0c7e769bb1f22798a815b
Shivam Bisht <rajshivam2745@gmail.com>
2025-08-11 23:56:31 +0530

    relation added for files

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,13 +1,40 @@
-import { integer, pgTable, varchar, text, uuid, boolean } from "drizzle-orm/pg-core"
+import { integer, pgTable, varchar, text, uuid, boolean, timestamp } from "drizzle-orm/pg-core"
 import { relations } from "drizzle-orm"
 
 export const files = pgTable("files", {
     id: uuid("id").defaultRandom().primaryKey(),
     name: text("name").notNull(),
     path: text("path").notNull(),
-    size:integer("size").notNull(),
-    type:text("type").notNull(), // "folder"
+    size: integer("size").notNull(),
+    type: text("type").notNull(),
 
-    
+    // storage
+    fileUrl: text("field_url").notNull(),
+    thumbmailUrl: text("thumbnail_url"),
+
+    // who belongs to who
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"),// parent folder if null for root  
+
+    // file/folder flags
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+    // timestamps
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
 
 })
+// relations drizzle orm
+export const filesRelations = relations(files, ({ one, many }) => ({
+    parent: one(files, {
+        fields: [files.parentId], //acts as a foreign key
+        references[files.id],
+    })
+
+
+        // could be many file inside the folder 
+        children: many(files)
+}))
